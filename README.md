ba5a44c382ec06cce7ebdd6d0dffe83ab4f1af0f
Vitaly Shalem <vitaly.shalem@gmail.com>
2023-06-23 22:40:37 +0200

    MVP version... README.md to be updated...

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# WikipediaScraper
+# Wikipedia Scraper
 BeCode project on web scraping
+
+## Description 
+(What is the project about? What is the goal? What is your approach?)
+
+## Installation 
+(What are the requirements to use your code?)
+
+ ## Usage 
+(How does someone use your code?)
+
+ ## Timeline 
+(How long did the project take?)
+
+## Personal situation 
+(e.g. This project was done as part of the AI Boocamp at BeCode.org.)
---
+++ utils/leaders_scraper.py
@@ -0,0 +1,76 @@
+# ##################################################################
+#  project:     wikipedia scraper
+#  date:        june 2023
+#  author:      vitaly shalem
+#  description: a project @ becode
+#               see more details in readme.md
+# ##################################################################
+
+import re
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def get_text(url, cookie, sn):
+    """ Gets text from wiki url;
+        Returns content and status """
+    wp = sn.get(url, cookies=cookie)
+    wpc = wp.text
+    return wpc, wp.status_code
+
+def get_first_paragraph(wiki_url, sn):
+    """ Gets the first paragraph from wiki page and cleans it """
+    content = None  # empty
+    cookie = None   # empty
+    status = 403    # no cookie by defaukt
+    # get text, try untill status is ok, if no cookie - create one
+    while status != 200:
+        if status == 403:
+            cookie = requests.get(wiki_url).cookies
+        content, status = get_text(wiki_url, cookie, sn)
+    # make soup
+    s = BeautifulSoup(content, "html.parser")
+    paragraphs = s.find_all("p")
+    # find the correct 1st paragraph
+    first_paragraph = None
+    for p in paragraphs:
+        if re.search(r"[ ]", str(p)):
+            if re.match(r"\<p\>([^\s]+ )?\<b\>", str(p)):
+                first_paragraph = p.text
+                break
+    # Cleaning the paragraph
+    first_paragraph = re.sub(r"\[((n )?[0-9]{1,2}|.)\]", "", first_paragraph)
+    first_paragraph = re.sub(r"(\(listen\)|[ ]?\(Écouter\)|([  ];)? Écouter)", "", first_paragraph)
+    first_paragraph = re.sub(r"[  ]uitspraak \(info / uitleg\)", "", first_paragraph)
+    first_paragraph = re.sub(r" \([0-9]{4}[-/][0-9]{2}[-/][0-9]{2}\)", "", first_paragraph)
+    first_paragraph = re.sub(r"(?<=[\]\)/])[  ]+(?=[.!?:;])", "", first_paragraph)
+    first_paragraph = re.sub(r"[ ]?\([  ]?\)", "", first_paragraph)
+    first_paragraph = re.sub(r"[  ]{1,}", " ", first_paragraph)
+    first_paragraph = first_paragraph.strip()
+    return first_paragraph
+
+def get_leaders():
+    """ Gets the list of leaders per country;
+        Finds the first paragraph of a wiki page for each leader;
+        Updates leaders per country data """
+    url = "https://country-leaders.onrender.com/"
+    # make cookie
+    cookie = requests.get(url+"cookie").cookies
+    # retrieve country codes
+    countries = requests.get(url+"countries", cookies=cookie).json()
+    # retrieve leaders informations
+    lpc = {c:requests.get(url+"leaders", cookies=cookie, params={"country": c}).json() for c in countries}
+    # create session
+    sn = requests.Session()
+    sn.get("https://www.wikipedia.org/")
+    for key, vList in lpc.items():
+        for index, vDict in enumerate(vList):
+            lpc[key][index]["first_paragraph"] = get_first_paragraph(vDict["wikipedia_url"], sn)
+    return lpc
+
+def save(lpc):
+    """ Gets a dictionary with data and saves it as json file """
+    output = "leaders.json"
+    with open(output, "w", encoding="utf-8") as json_file:
+        json_file.write(json.dumps(lpc, indent=4))
+    return output
